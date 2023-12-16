07649eb3e95cc8268af70ed70ae6e235f7515756
xitanii <146194283+xitanii@users.noreply.github.com>
2023-12-16 07:27:35 +0200

    11 laba

---
+++ arkadiy/11 laba/laba 11 1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+int sumatsifr(int number) {
+    int sum = 0;
+    while (number != 0) {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void ubivanie(int* chislo, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (chislo[j] < chislo[j + 1]) {
+                swap(chislo[j], chislo[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    std::cout << "Enter the number of elements: ";
+    std::cin >> n;
+    int* chislo = new int[n];
+
+    std::cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        std::cin >> chislo[i];
+    }
+
+    bool hasSum19 = false;
+    for (int i = 0; i < n; i++) {
+        if (sumatsifr(chislo[i]) == 19) {
+            hasSum19 = true;
+            break;
+        }
+    }
+
+    if (!hasSum19) {
+        ubivanie(chislo, n);
+        for (int i = 0; i < n; i++) {
+            std::cout << chislo[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+    delete[] chislo;
+
+    return 0;
+}
---
+++ arkadiy/11 laba/laba 11 3.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+
+    int** matrix = new int* [n];
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[m];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            std::cin >> matrix[i][j];
+        }
+    }
+
+    int maxSum = 0;
+    int maxSumRow = 0;
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = 0; j < m; j++) {
+            sum += abs(matrix[i][j]);
+        }
+        if (sum > maxSum) {
+            maxSum = sum;
+            maxSumRow = i;
+        }
+    }
+
+    for (int j = 0; j < m; j++) {
+        matrix[maxSumRow][j] = 9999;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < n; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
+    return 0;
+}
